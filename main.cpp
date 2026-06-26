40fb3974c37cfd86d57039fa9b890e2f4736042e
r3d <jose.alva.e@utec.edu.pe>
2026-06-26 11:53:14 -0500

    minor minor fix

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-
+using namespace std;
 int main(int argc, char *argv[]) {
   // Verificamos que se pase el archivo binario como argumento
   if (argc != 2) {
